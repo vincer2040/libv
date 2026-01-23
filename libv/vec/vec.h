1ece3f588b651c6ebb97ebbad7519612869a9ef4
vincer2040 <ronnebaumvince@gmail.com>
2026-01-23 12:33:39 -0500

    give same interface as cpp vec

--- libv/vec/vec.h
+++ libv/vec/vec.h
@@ -34,6 +34,8 @@ typedef struct {
     const vec_object_policy* obj;
 } vec_policy;
 
+// init / destroy
+
 static inline vec_raw vec_raw_new(void) {
     vec_raw self = {0};
     return self;
@@ -48,6 +50,12 @@ static inline void vec_raw_free(const vec_policy* policy, vec_raw* self) {
     policy->alloc->free(self->data);
     self->size = 0;
     self->capacity = 0;
+}
+
+// capacity
+
+static inline bool vec_raw_empty(const vec_raw* self) {
+    return self->size == 0;
 }
 
 static inline size_t vec_raw_size(const vec_raw* self) { return self->size; }
@@ -85,32 +93,24 @@ static inline int vec_raw_reserve(const vec_policy* policy, vec_raw* self,
     return vec_raw_realloc_self(policy, self, capacity);
 }
 
-static inline int vec_raw_push_back(const vec_policy* policy, vec_raw* self,
-                                    const void* value) {
-    if (vec_raw_maybe_resize(policy, self) == LIBV_ERR) {
-        return LIBV_ERR;
-    }
-    policy->obj->copy(self->data + (self->size * policy->obj->size), value);
-    self->size++;
-    return LIBV_OK;
-}
-
-static inline int vec_raw_push_front(const vec_policy* policy, vec_raw* self,
-                                     const void* value) {
-    if (vec_raw_maybe_resize(policy, self) == LIBV_ERR) {
-        return LIBV_ERR;
-    }
-    if (self->size == 0) {
-        policy->obj->copy(self->data, value);
-        self->size++;
+static inline int vec_raw_shrink_to_size(const vec_policy* policy,
+                                         vec_raw* self) {
+    if (self->size == self->capacity) {
         return LIBV_OK;
     }
-    memmove(self->data + policy->obj->size, self->data,
-            self->size * policy->obj->size);
-    policy->obj->copy(self->data, value);
-    self->size++;
-    return LIBV_OK;
+    return vec_raw_realloc_self(policy, self, self->size);
 }
+
+static inline void vec_raw_clear(const vec_policy* policy, vec_raw* self) {
+    if (policy->obj->dtor) {
+        for (size_t i = 0; i < self->size; ++i) {
+            policy->obj->dtor(self->data + (i * policy->obj->size));
+        }
+    }
+    self->size = 0;
+}
+
+// access
 
 static inline const void* vec_raw_get_at_unchecked(const vec_policy* policy,
                                                    const vec_raw* self,
@@ -126,21 +126,26 @@ static inline const void* vec_raw_get_at(const vec_policy* policy,
     return vec_raw_get_at_unchecked(policy, self, index);
 }
 
-static inline int vec_raw_pop_back(const vec_policy* policy, vec_raw* self,
-                                   void* out) {
-    if (self->size == 0) {
-        return LIBV_ERR;
-    }
-    self->size--;
-    if (out) {
-        policy->obj->copy(out, self->data + (self->size * policy->obj->size));
-    } else {
-        if (policy->obj->dtor) {
-            policy->obj->dtor(self->data + (self->size * policy->obj->size));
-        }
-    }
-    return LIBV_OK;
+static inline const void* vec_raw_data(const vec_raw* self) {
+    return self->data;
 }
+
+static inline const void* vec_raw_front(const vec_raw* self) {
+    if (self->size == 0) {
+        return NULL;
+    }
+    return self->data;
+}
+
+static inline const void* vec_raw_back(const vec_policy* policy,
+                                       const vec_raw* self) {
+    if (self->size == 0) {
+        return NULL;
+    }
+    return self->data + ((self->size - 1) * policy->obj->size);
+}
+
+// modification
 
 static inline void vec_raw_remove_at_unchecked(const vec_policy* policy,
                                                vec_raw* self, size_t index,
@@ -174,6 +179,66 @@ static inline int vec_raw_pop_front(const vec_policy* policy, vec_raw* self,
                                     void* out) {
     return vec_raw_remove_at(policy, self, 0, out);
 }
+
+static inline int vec_raw_push_front(const vec_policy* policy, vec_raw* self,
+                                     const void* value) {
+    if (vec_raw_maybe_resize(policy, self) == LIBV_ERR) {
+        return LIBV_ERR;
+    }
+    if (self->size == 0) {
+        policy->obj->copy(self->data, value);
+        self->size++;
+        return LIBV_OK;
+    }
+    memmove(self->data + policy->obj->size, self->data,
+            self->size * policy->obj->size);
+    policy->obj->copy(self->data, value);
+    self->size++;
+    return LIBV_OK;
+}
+
+static inline int vec_raw_pop_back(const vec_policy* policy, vec_raw* self,
+                                   void* out) {
+    if (self->size == 0) {
+        return LIBV_ERR;
+    }
+    self->size--;
+    if (out) {
+        policy->obj->copy(out, self->data + (self->size * policy->obj->size));
+    } else {
+        if (policy->obj->dtor) {
+            policy->obj->dtor(self->data + (self->size * policy->obj->size));
+        }
+    }
+    return LIBV_OK;
+}
+
+static inline int vec_raw_push_back(const vec_policy* policy, vec_raw* self,
+                                    const void* value) {
+    if (vec_raw_maybe_resize(policy, self) == LIBV_ERR) {
+        return LIBV_ERR;
+    }
+    policy->obj->copy(self->data + (self->size * policy->obj->size), value);
+    self->size++;
+    return LIBV_OK;
+}
+
+static inline int vec_raw_append(const vec_policy* policy, vec_raw* self,
+                                 const vec_raw* other) {
+    if (vec_raw_reserve(policy, self, self->size + other->size) == LIBV_ERR) {
+        return LIBV_ERR;
+    }
+    for (size_t i = 0; i < other->size; ++i) {
+        if (vec_raw_push_back(policy, self,
+                              other->data + (i * policy->obj->size)) ==
+            LIBV_ERR) {
+            return LIBV_ERR;
+        }
+    }
+    return LIBV_OK;
+}
+
+// iter
 
 typedef struct {
     vec_raw* vec;
@@ -229,6 +294,9 @@ static inline void vec_raw_iter_next(vec_raw_iter* self) { self->position++; }
     static inline void name_##_free(name_* self) {                             \
         vec_raw_free(&policy_, &self->vec);                                    \
     }                                                                          \
+    static inline bool name_##_empty(const name_* self) {                      \
+        return vec_raw_empty(&self->vec);                                      \
+    }                                                                          \
     static inline size_t name_##_size(const name_* self) {                     \
         return vec_raw_size(&self->vec);                                       \
     }                                                                          \
@@ -238,8 +306,39 @@ static inline void vec_raw_iter_next(vec_raw_iter* self) { self->position++; }
     static inline int name_##_reserve(name_* self, size_t new_capacity) {      \
         return vec_raw_reserve(&policy_, &self->vec, new_capacity);            \
     }                                                                          \
-    static inline int name_##_push_back(name_* self, type_* value) {           \
-        return vec_raw_push_back(&policy_, &self->vec, value);                 \
+    static inline int name_##_shrink_to_size(name_* self) {                    \
+        return vec_raw_shrink_to_size(&policy_, &self->vec);                   \
+    }                                                                          \
+    static inline void name_##_clear(name_* self) {                            \
+        vec_raw_clear(&policy_, &self->vec);                                   \
+    }                                                                          \
+    static inline const type_* name_##_get_at_unchecked(const name_* self,     \
+                                                        size_t index) {        \
+        return vec_raw_get_at_unchecked(&policy_, &self->vec, index);          \
+    }                                                                          \
+    static inline const type_* name_##_get_at(const name_* self,               \
+                                              size_t index) {                  \
+        return vec_raw_get_at(&policy_, &self->vec, index);                    \
+    }                                                                          \
+    static inline const type_* name_##_data(const name_* self) {               \
+        return vec_raw_data(&self->vec);                                       \
+    }                                                                          \
+    static inline const type_* name_##_front(const name_* self) {              \
+        return vec_raw_front(&self->vec);                                      \
+    }                                                                          \
+    static inline const type_* name_##_back(const name_* self) {               \
+        return vec_raw_back(&policy_, &self->vec);                             \
+    }                                                                          \
+    static inline void name_##_remove_at_unchecked(name_* self, size_t index,  \
+                                                   void* out) {                \
+        vec_raw_remove_at_unchecked(&policy_, &self->vec, index, out);         \
+    }                                                                          \
+    static inline int name_##_remove_at(name_* self, size_t index,             \
+                                        void* out) {                           \
+        return vec_raw_remove_at(&policy_, &self->vec, index, out);            \
+    }                                                                          \
+    static inline int name_##_pop_front(name_* self, type_* out) {             \
+        return vec_raw_pop_front(&policy_, &self->vec, out);                   \
     }                                                                          \
     static inline int name_##_push_front(name_* self, type_* value) {          \
         return vec_raw_push_front(&policy_, &self->vec, value);                \
@@ -247,24 +346,11 @@ static inline void vec_raw_iter_next(vec_raw_iter* self) { self->position++; }
     static inline int name_##_pop_back(name_* self, type_* out) {              \
         return vec_raw_pop_back(&policy_, &self->vec, out);                    \
     }                                                                          \
-    static inline int name_##_pop_front(name_* self, type_* out) {             \
-        return vec_raw_pop_front(&policy_, &self->vec, out);                   \
+    static inline int name_##_push_back(name_* self, type_* value) {           \
+        return vec_raw_push_back(&policy_, &self->vec, value);                 \
     }                                                                          \
-    static inline const type_* name_##_get_at(const name_* self,               \
-                                              size_t index) {                  \
-        return vec_raw_get_at(&policy_, &self->vec, index);                    \
-    }                                                                          \
-    static inline const type_* name_##_get_at_unchecked(const name_* self,     \
-                                                        size_t index) {        \
-        return vec_raw_get_at_unchecked(&policy_, &self->vec, index);          \
-    }                                                                          \
-    static inline int name_##_remove_at(name_* self, size_t index,             \
-                                        void* out) {                           \
-        return vec_raw_remove_at(&policy_, &self->vec, index, out);            \
-    }                                                                          \
-    static inline void name_##_remove_at_unchecked(name_* self, size_t index,  \
-                                                   void* out) {                \
-        vec_raw_remove_at_unchecked(&policy_, &self->vec, index, out);         \
+    static inline int name_##_append(name_* self, const name_* other) {        \
+        return vec_raw_append(&policy_, &self->vec, &other->vec);              \
     }                                                                          \
     typedef struct {                                                           \
         vec_raw_iter it;                                                       \
